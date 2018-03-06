b5803934b5c95c1b8f82795c39f2200c4f071484
guzzilar <iam.nuttanon@gmail.com>
2018-03-07 02:23:05 +0700

    First initial plugin.

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WooChef: Dashboard
+ * Plugin URI:  https://github.com/woochef/dashboard
+ * Description: A customizable admin dashboard that helping you monitoring every movement in your WooCommerce store.
+ * Version:     0.0.1
+ * Author:      WooChef and contributors
+ * Author URI:  https://github.com/woochef/dashboard/graphs/contributors
+ * Text Domain: woochef-dashboard
+ * License:     MIT
+ * License URI: https://opensource.org/licenses/MIT
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
